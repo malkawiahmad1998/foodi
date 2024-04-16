db4884c69930ef3d2cd6c9af0c4cc03ae16bf191
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-04-16 14:55:15 +0300

    cards

---
+++ js/cards.js
@@ -0,0 +1,64 @@
+var image = document.getElementById('image');
+var title = document.getElementById('title');
+var description = document.getElementById('description');
+var price = document.getElementById('price');
+var AddNewCardFormBtn = document.getElementById('AddNewCardFormBtn');
+var cardsBody = document.getElementById('cardsBody')
+
+
+addNewCardForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    //image
+    var newImage = document.createElement('img');
+    newImage.className = 'card-img-top';
+    newImage.src = image.value
+    //title
+    var newTitle = document.createElement('h5');
+    newTitle.className = 'card-title';
+    newTitle.innerHTML = title.value;
+    //description
+    var newDescription = document.createElement('p');
+    newDescription.className = 'card-text';
+    newDescription.innerHTML = description.value;
+    //price
+    var newPrice = document.createElement('a');
+    newPrice.className = 'btn-primary';
+    newPrice.innerHTML = price.value;
+    
+    var content = document.createElement('div');
+    content.className = 'card-body';
+
+    var card = document.createElement('div');
+    card.className = 'card';
+
+    //deleteButton
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn-primary'
+    deleteBtn.innerHTML ='Delete'
+
+     //updateButton
+     var updateBtn = document.createElement('button');
+     updateBtn.className = 'btn-primary'
+     updateBtn.innerHTML ='Update'
+ 
+
+    //content_card
+    content.append(newTitle)
+    content.append(newDescription)
+    content.append(newPrice)
+
+    //card
+    card.append(newImage)
+    card.append(content)
+    card.append(deleteBtn)
+    card.append(updateBtn)
+    cardsBody.append(card)
+    card.style.width ='18rem'
+    
+    
+
+
+    
+
+})
+
