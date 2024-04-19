cc1b48d4afdf5ba021a5ee46ca8c2058c42494a7
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-04-19 14:50:58 +0300

    update

---
+++ js/cardsFlip.js
@@ -0,0 +1,7 @@
+let inputForm = document.getElementById('inputForm');
+let name = document.getElementById('name');
+let major = document.getElementById('major');
+let profile = document.getElementById('profile');
+
+
+console.log(inputForm,name,major,profile)
