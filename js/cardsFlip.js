e236e8e2463bbce966b20d1e75f926bdc425f18f
Ahmad Malkawi <malkawi_ahmad@hotmail.com>
2024-04-20 05:43:27 +0300

    update

--- js/cardsFlip.js
+++ js/cardsFlip.js
@@ -1,7 +1,15 @@
-let inputForm = document.getElementById('inputForm');
-let name = document.getElementById('name');
-let major = document.getElementById('major');
-let profile = document.getElementById('profile');
+var flipBtn = document.getElementsByClassName('flipBtn');
+var cardTitle = document.getElementsByClassName('card-title');
+var cardText = document.getElementsByClassName('card-text')
+var cardLink = document.getElementsByClassName('cardLink')
+var faceORback = document.getElementsByClassName('face')
 
+function flipTheCard(index) {
+    if(faceORback[index].innerHTML == 'face'){
+        cardTitle[index].innerHTML ='name back';
+        faceORback[index].innerHTML == 'back'
 
-console.log(inputForm,name,major,profile)
+    }else{
+        
+    }
+}
